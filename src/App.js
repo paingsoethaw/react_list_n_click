783286b4c088e4f202c5fa9850ef0b0ccf7cf543
paing_soe_thaw <paingsoethaw@gmail.com>
2018-05-17 10:39:18 +0800

    Component list with map

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+var people = [
+  {
+    name: "David",
+    age: 24,
+    id: 1
+  },
+  {
+    name: "John",
+    age: 22,
+    id: 2
+  }
+];
+
+class Card extends Component {
+  render() {
+    return (
+      <div>
+        <h2>{this.props.name}</h2>
+        <p>Age: {this.props.age}</p>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        {people.map(function(person) {
+          return <Card name={person.name} age={person.age} />;
+        })}
       </div>
     );
   }
